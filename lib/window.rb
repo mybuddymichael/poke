8458e163c2ce2c41ad6eb9a179daab3adab0d667
Michael Hanson <michael.b.hanson@gmail.com>
2011-05-22 14:52:29 -0700

    Add explanatory comment

--- lib/window.rb
+++ lib/window.rb
@@ -13,6 +13,7 @@ class GameWindow < Gosu::Window
 	end
 	
 	def update
+		# This is probably unnecessary and but nil scares the hell out of me.
 		@buttons_pushed.uniq! if @buttons_pushed.uniq!
 
 		case @buttons_pushed.last

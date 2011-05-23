a7a5d4fea956cef5fc0aa5cc55c62dc3cf338064
Michael Hanson <michael.b.hanson@gmail.com>
2011-05-22 17:33:20 -0700

    Refactor player.rb to place logic in the update method

--- lib/player.rb
+++ lib/player.rb
@@ -8,16 +8,34 @@ class Player
 		@current_image = @facing_down
 		@x, @y = x, y
 		@name = name
+
+		@movement_factor = 4
 		@position_range = {xmin: 0, xmax: 480-16, ymin: 0, ymax: 320-16}.freeze
 	end
 
 	def update
 		case @direction
-			when :up    then @current_image = @facing_up
-			when :down  then @current_image = @facing_down
-			when :left  then @current_image = @facing_left
-			when :right then @current_image = @facing_right
+			when :up
+				@current_image = @facing_up
+				@y -= @movement_factor
+			when :down
+				@current_image = @facing_down
+				@y += @movement_factor
+			when :left
+				@current_image = @facing_left
+				@x -= @movement_factor
+			when :right
+				@current_image = @facing_right
+				@x += @movement_factor
 		end
+
+		if @x < @position_range[:xmin] then @x = @position_range[:xmin] end
+		if @x > @position_range[:xmax] then @x = @position_range[:xmax] end
+		if @y < @position_range[:ymin] then @y = @position_range[:ymin] end
+		if @y > @position_range[:ymax] then @y = @position_range[:ymax] end
+		
+		# This makes the player stop when the button is released
+		@direction = :no_direction
 	end
 
 	def draw
@@ -26,17 +44,6 @@ class Player
 
 	def move(direction)
 		@direction = direction
-		case direction
-			when :left  then @x -= 4
-			when :right then @x += 4
-			when :up    then @y -= 4
-			when :down  then @y += 4
-		end
-
-		if @x < @position_range[:xmin] then @x = @position_range[:xmin] end
-		if @x > @position_range[:xmax] then @x = @position_range[:xmax] end
-		if @y < @position_range[:ymin] then @y = @position_range[:ymin] end
-		if @y > @position_range[:ymax] then @y = @position_range[:ymax] end
 	end
 
 end

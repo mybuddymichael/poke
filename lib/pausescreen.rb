e22950705eafa95ba992becded961a35929ef9de
Michael Hanson <michael.b.hanson@gmail.com>
2011-05-24 22:59:37 -0700

    Add pause_text variable

--- lib/pausescreen.rb
+++ lib/pausescreen.rb
@@ -1,9 +1,11 @@
 class PauseScreen
 
 	def initialize(window, window_width, window_height)
-		@pause_button = Gosu::Image.new(window, 'media/pause_button.png', true)
 		@window_width, @window_height = window_width, window_height
 		@window = window
+
+		@pause_button = Gosu::Image.new(window, 'media/pause_button.png', true)
+		@pause_text   = Gosu::Image.from_text(window, 'Paused', 'Poke', 16)
 	end
 
 	def draw

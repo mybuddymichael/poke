403feda27d8d19fa4929e12b8500830eba41728f
Michael Hanson <michael.b.hanson@gmail.com>
2011-05-24 22:54:21 -0700

    Add window reference, height and width to PauseScreen.new

--- lib/pausescreen.rb
+++ lib/pausescreen.rb
@@ -1,7 +1,9 @@
 class PauseScreen
 
-	def initialize
-		@pause_button = Gosu::Image.new(self, 'media/pause_button.png', true)
+	def initialize(window, window_width, window_height)
+		@pause_button = Gosu::Image.new(window, 'media/pause_button.png', true)
+		@window_width, @window_height = window_width, window_height
+		@window = window
 	end
 
 end

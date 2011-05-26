966a0dab81def21ae3830419983fe0b62f900a30
Michael Hanson <michael.b.hanson@gmail.com>
2011-05-26 07:13:48 -0700

    Add refute_nil test for PauseScreen

--- spec.rb
+++ spec.rb
@@ -14,6 +14,19 @@ class TestWindow < MiniTest::Unit::TestCase
 
 end
 
+class TestPauseScreeen < MiniTest::Unit::TestCase
+
+	def setup
+		@window = GameWindow.new
+		@pause_screen = PauseScreen.new(@window, 480, 320)
+	end
+
+	def test_that_pause_screen_is_created
+		refute_nil(@pause_screen)
+	end
+
+end
+
 class TestPlayer < MiniTest::Unit::TestCase
 
 	def setup

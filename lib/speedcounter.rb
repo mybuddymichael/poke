65de999dfe12a42f36164cd4f45f8662e17d8938
Michael Hanson <michael.b.hanson@gmail.com>
2011-05-28 08:55:52 -0700

    Change SpeedCounter name to more appropriate Coordinates

--- lib/speedcounter.rb
+++ lib/speedcounter.rb
@@ -1,8 +1,8 @@
-class SpeedCounter
+class Coordinates
 
 	def initialize(window, player)
 		@window, @player = window, player
-		@speed_text = Gosu::Font.new(@window, 'media/poke.ttf', 18)
+		@coordinate_text = Gosu::Font.new(@window, 'media/poke.ttf', 18)
 	end
 
 	def update
@@ -10,7 +10,7 @@ class SpeedCounter
 	end
 
 	def draw
-		@speed_text.draw("#{@x.to_s}, #{@y.to_s}", 400, 300, 1, 1, 1, Colors::Black,
+		@coordinate_text.draw("#{@x.to_s}, #{@y.to_s}", 400, 300, 1, 1, 1, Colors::Black,
 		                 :default)
 	end
 

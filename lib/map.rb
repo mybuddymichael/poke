1444e05f51827d22d0f0b01a965442a35ee045c6
Michael Hanson <michael.b.hanson@gmail.com>
2011-05-29 15:14:16 -0700

    Call get_lines in initialize

--- lib/map.rb
+++ lib/map.rb
@@ -4,6 +4,8 @@ class Map
     @window = window
     @tileset = Gosu::Image.load_tiles(window, 'media/32x32diagonal_lines.png',
                                       32, 32, false)
+
+    get_lines(mapfile)
   end
 
   def get_lines(file)

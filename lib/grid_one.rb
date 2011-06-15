62839504e2db8a62340a59cb848e47e7b9270061
Michael Hanson <michael.b.hanson@gmail.com>
2011-06-15 12:30:33 -0700

    Update GridOne to subclass Grid

--- lib/grid_one.rb
+++ lib/grid_one.rb
@@ -1,42 +1,21 @@
-class GridOne
+class GridOne < Grid
 
-  attr_reader :map, :program_1, :background
+  attr_reader :program_1
 
-  def initialize(window, user)
-    @window, @user = window, user
-    @map = Map.new(@window, 'media/grid_one.txt', 'media/grid_one_tileset.png',
-                   {'V'=>0, 'g'=>1, '.'=>nil})
-    @background  = Gosu::Image.new(@window, 'media/background.png', false)
+  def initialize
+    super
     @program_1   = Program.new(@window, 416, 160)
   end
 
-  def start
-    @window.set_current_grid_as(self)
-  end
 
   def update
+    super
     @program_1.update
   end
 
   def draw
-    @map.draw
+    super
     @program_1.draw
-  end
-
-  def width
-    @map.width
-  end
-
-  def height
-    @map.height
-  end
-
-  def solid_blocks
-    @map.solid_blocks
-  end
-
-  def lines
-    @map.lines
   end
 
 end

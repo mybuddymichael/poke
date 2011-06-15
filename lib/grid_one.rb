c3c45b50dc721e878236308f2ee0258536db053f
Michael Hanson <michael.b.hanson@gmail.com>
2011-06-14 23:22:08 -0700

    Use grid_one.rb to contain first map and npc instances

--- lib/grid_one.rb
+++ lib/grid_one.rb
@@ -1,11 +1,33 @@
 class GridOne
 
-  def initialize(window, player)
-    @window, @player = window, player
+  attr_reader :map, :user, :program_1, :background
+
+  def initialize(window, user)
+    @window, @user = window, user
   end
   
   def start
-    @map = Map.new(self, 'media/grid_one.txt', 'media/
+    @map = Map.new(@window, 'media/grid_one.txt', 'media/grid_one_tileset.png',
+                   {'V'=>0, 'g'=>1, '.'=>nil})
+    @background  = Gosu::Image.new(@window, 'media/background.png', false)
+    @program_1   = Program.new(@window, 416, 160)
+  end
+
+  def update
+    @program_1.update
+  end
+
+  def draw
+    @map.draw
+    @program_1.draw
+  end
+
+  def width
+    @map.width
+  end
+
+  def height
+    @map.height
   end
 
 end

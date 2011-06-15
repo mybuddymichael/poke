a969679082865991fa207339b5ec375388ba5900
Michael Hanson <michael.b.hanson@gmail.com>
2011-06-15 13:36:51 -0700

    Move parameters to GridOne from window

--- lib/grid_one.rb
+++ lib/grid_one.rb
@@ -2,8 +2,10 @@ class GridOne < Grid
 
   attr_reader :program_1
 
-  def initialize
-    super
+  def initialize(window, user)
+    super(window, user, 'media/grid_one.txt',
+          'media/grid_one_tileset.png', {'V'=>0, 'g'=>1, '.'=>nil},
+          'media/background.png')
     @program_1   = Program.new(@window, 416, 160)
   end
 

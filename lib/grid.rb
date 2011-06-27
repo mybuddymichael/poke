1edd902123840c0b232842b8809788b4f39cfc3b
Michael Hanson <michael.b.hanson@gmail.com>
2011-06-27 14:32:53 -0700

    Add rough skeleton of Grid#create_programs method

--- lib/grid.rb
+++ lib/grid.rb
@@ -3,6 +3,8 @@ class Grid
   def initialize(window, user, mapfile, tileset, program_file=nil)
     @window, @user = window, user
     @map = Map.new(window, mapfile, tileset)
+
+    create_programs(program_file)
   end
 
   def draw
@@ -23,6 +25,27 @@ class Grid
 
   def lines
     @map.lines
+  end
+
+  private
+
+  def create_programs(program_file)
+    @program_map = ProgramMap.new(program_file)
+    n = 0
+    programs = []
+
+    @program_map.lines.each do |line|
+      if line.include?('s')
+        n += 1
+        programs.push(n)
+      end
+    end
+
+    programs.each do |number|
+      id = 'program_' + number.to_s
+      instance_variable_set("@#{id}", Program.new)
+    end
+
   end
 
 end

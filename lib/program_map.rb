d591b3f8057518c80208eb94d96bb772e9710ceb
Michael Hanson <michael.b.hanson@gmail.com>
2011-06-28 07:23:11 -0700

    Some interesting logic for get_paths

--- lib/program_map.rb
+++ lib/program_map.rb
@@ -15,15 +15,37 @@ class ProgramMap
   def get_paths
     @height.times do |y|
       @width.times do |x|
-        if @lines[y][x] == ('p' or 's') and @lines[y][x-1] == '.'
-          start_path(x, y)
+        if @lines[y][x] == ('p' or 's')
+          if @lines[y][x-1] == '.' # => Left boundary.
+            if @lines[y-1][x] == '.' # => Upper boundary.
+              mark_upper_left_boundary(x, y)
+            elsif @lines[y-1][x] == '.' and @lines[y+1][x] == '.'
+              # => Upper and lower boundary.
+              mark_upper_left_boundary(x, y)
+              mark_bottom_left_boundary(x, y)
+            elsif @lines[y+1][x] == '.' # => Lower boundary.
+              mark_bottom_left_boundary(x,y)
+            end
+          elsif @lines[y][x+1] == '.' # => Right boundary.
+            if @lines[y-1][x] == '.' # => Upper boundary.
+              mark_upper_right_boundary(x+1, y)
+            end
+          end
         end
       end
     end
   end
 
-  def start_path(x, y)
-    @boundary[:up => y, :left => x]
+  def mark_upper_left_boundary(x, y)
+    @boundary[up: y, left: x]
+  end
+
+  def mark_upper_right_boundary(x, y)
+    @boundary[up: y, right: (x+1)]
+  end
+
+  def mark_bottom_left_boundary(x, y)
+    @boundary[down: (y+1), left: x]
   end
 
 end

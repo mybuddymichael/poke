c2619fc0aeb9365b5dd36b001df4cab0610d89be
Michael Hanson <michael.b.hanson@gmail.com>
2011-06-27 21:14:20 -0700

    Start of ProgramMap::get_paths

--- lib/program_map.rb
+++ lib/program_map.rb
@@ -6,6 +6,24 @@ class ProgramMap
 
   def initialize(pathfile)
     get_lines(pathfile)
+    get_width
+    get_height
+  end
+
+  private
+
+  def get_paths
+    @height.times do |y|
+      @width.times do |x|
+        if @lines[y][x] == ('p' or 's') and @lines[y][x-1] == '.'
+          start_path(x, y)
+        end
+      end
+    end
+  end
+
+  def start_path(x, y)
+    @boundary[:up => y, :left => x]
   end
 
 end

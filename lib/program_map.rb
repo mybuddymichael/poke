d75b40b6e14d80a64f990976f6bca990c5a029ee
Michael Hanson <michael.b.hanson@gmail.com>
2011-07-15 19:43:56 -0700

    Make ProgramMap x and y instance variables so they can be easily accessed

--- lib/program_map.rb
+++ lib/program_map.rb
@@ -14,7 +14,12 @@ class ProgramMap
 
   def get_paths
     @width.times do |x|
+
+      @x = x
+
       @height.times do |y|
+
+        @y = y
 
         next if current_cell == "."
 
@@ -34,39 +39,39 @@ class ProgramMap
   end
 
   def current_cell
-    @map_in_columns[x][y]
+    @map_in_columns[@x][@y]
   end
 
   def above
-    @map_in_columns[x][y-1]
+    @map_in_columns[@x][@y-1]
   end
 
   def below
-    @map_in_columns[x][y+1]
+    @map_in_columns[@x][@y+1]
   end
 
   def to_the_left
-    @map_in_columns[x-1][y]
+    @map_in_columns[@x-1][@y]
   end
 
   def to_the_right
-    @map_in_columns[x+1][y]
+    @map_in_columns[@x+1][@y]
   end
 
-  def mark_upper_left_boundary(x, y)
-    @boundary[up: y, left: x]
+  def mark_upper_left_boundary(@x, @y)
+    @boundary[up: @y, left: @x]
   end
 
-  def mark_upper_right_boundary(x, y)
-    @boundary[up: y, right: (x+1)]
+  def mark_upper_right_boundary(@x, @y)
+    @boundary[up: @y, right: (@x+1)]
   end
 
-  def mark_bottom_left_boundary(x, y)
-    @boundary[down: (y+1), left: x]
+  def mark_bottom_left_boundary(@x, @y)
+    @boundary[down: (@y+1), left: @x]
   end
 
-  def mark_bottom_right_boundary(x, y)
-    @boundary[down: (y+1), right: (x+1)]
+  def mark_bottom_right_boundary(@x, @y)
+    @boundary[down: (@y+1), right: (@x+1)]
   end
 
 end

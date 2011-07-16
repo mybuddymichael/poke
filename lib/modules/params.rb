aa6f94e4327202f66704de521f4f5af1b2dbace2
Michael Hanson <michael.b.hanson@gmail.com>
2011-07-16 14:20:15 -0700

    Make Params#check_params a class method

--- lib/modules/params.rb
+++ lib/modules/params.rb
@@ -2,7 +2,7 @@ module Params
 
   WrongNumberOfParamsError = Class.new(StandardError)
 
-  def check_params(params_given, params_required)
+  def Params.check_params(params_given, params_required)
     params_required.each do |param|
       unless params_given.keys.include?(param)
         raise WrongNumberOfParamsError,

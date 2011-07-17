c8bcd9c8a5698b664540651bf9ecc79f04bf2bd8
Michael Hanson <michael.b.hanson@gmail.com>
2011-07-16 22:56:47 -0700

    Fix black bar on bottom bug

--- lib/map.rb
+++ lib/map.rb
@@ -44,8 +44,8 @@ class Map
 
     map_key = eval(@map_in_lines[map_key_location])
 
-    @map_in_lines.delete_at(map_break)
     @map_in_lines.delete_at(map_key_location)
+    @map_in_lines.delete_at(map_break)
 
     map_key
   end

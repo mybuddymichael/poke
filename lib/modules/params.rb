71d255d4ce08719295a345bfc5efa1f0471cb2b1
Michael Hanson <michael.b.hanson@gmail.com>
2011-07-16 21:03:38 -0700

    Make #check_params a class method

--- lib/modules/params.rb
+++ lib/modules/params.rb
@@ -2,13 +2,17 @@ module Params
 
   MissingParamsError = Class.new(StandardError)
 
-  def Params.check_params(params_given, params_required)
-    params_required.each do |param|
-      unless params_given.keys.include?(param)
-        raise WrongNumberOfParamsError,
-          "parameters given do not include \":#{param}\""
+  class << self
+
+    def check_params(params_given, params_required)
+      params_required.each do |param|
+        unless params_given.keys.include?(param)
+          raise MissingParamsError,
+            "parameters given do not include \":#{param}\""
+        end
       end
     end
+
   end
 
 end

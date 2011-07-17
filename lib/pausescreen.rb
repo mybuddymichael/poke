dff34471af7d7e8ed38b95b9315813246099c3ec
Michael Hanson <michael.b.hanson@gmail.com>
2011-07-17 14:10:50 -0700

    In PauseScreen, remove window_ prefix from width, height variables

--- lib/pausescreen.rb
+++ lib/pausescreen.rb
@@ -1,17 +1,17 @@
 class PauseScreen
 
   def initialize(params)
-    Params.check_params(params, [:window, :window_width, :window_height]
-    @window        = params[:window]
-    @window_width  = params[:window_width]
-    @window_height = params[:window_height]
+    Params.check_params(params, [:window, :width, :height])
+    @window = params[:window]
+    @width  = params[:width]
+    @height = params[:height]
 
     @pause_button = Gosu::Image.new(@window, 'media/pause_button.png', false)
     @press_q      = Gosu::Image.new(@window, 'media/press_q.png', false)
   end
 
   def draw
-    draw_rect(@window_width, @window_height, Color::TRANS_BLACK,
+    draw_rect(@width, @height, Color::TRANS_BLACK,
               ZOrder::PAUSE_BACKGROUND)
     @pause_button.draw(160, 144, ZOrder::PAUSE_BUTTON)
     @press_q.draw(170, 296, ZOrder::PAUSE_BUTTON)

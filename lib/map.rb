2bda3aab573790bbd679337364dc03db12004a6c
Michael Hanson <michael.b.hanson@gmail.com>
2011-07-16 22:25:24 -0700

    Rename mapfile to map_file

--- lib/map.rb
+++ lib/map.rb
@@ -34,7 +34,7 @@ class Map
   def extract_map_key
     map_break = @lines.index('')
     if map_break == nil
-      raise IndexError, "There's no map break in #{@mapfile}"
+      raise IndexError, "There's no map break in #{@map_file}"
     end
 
     map_key = map_break+1

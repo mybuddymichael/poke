7fd882144abe54e3cc656795be3172138c35b1be
Michael Hanson <michael.b.hanson@gmail.com>
2011-07-24 13:55:40 -0700

    Add toggle_pause logic to Controls#release_button

--- lib/poke/controls.rb
+++ lib/poke/controls.rb
@@ -30,7 +30,11 @@ class Controls
   def release_button(button)
     BUTTONS.each do |key, value|
       if button == value
-        @buttons_pressed.delete(key)
+        if key == :escape
+          toggle_pause
+        else
+          @buttons_pressed.delete(key)
+        end
       end
     end
   end

a59120f08b25268ad8886c54f052b1c855262ac4
Michael Hanson <michael.b.hanson@gmail.com>
2011-07-24 12:14:10 -0700

    Add BUTTONS constant hash to Controls

--- lib/poke/controls.rb
+++ lib/poke/controls.rb
@@ -2,6 +2,11 @@ class Controls
 
   PARAMS_REQUIRED = [:window]
 
+  BUTTONS = { up:    Gosu::KbUp,
+              down:  Gosu::KbDown,
+              left:  Gosu::KbLeft,
+              right: Gosu::KbRight }
+
   def initialize(params)
     Params.check_params(params, PARAMS_REQUIRED)
     

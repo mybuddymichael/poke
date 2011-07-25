d83c3fc73b443c99746e0ee993cdb4e3a1172414
Michael Hanson <michael.b.hanson@gmail.com>
2011-07-25 13:12:11 -0700

    Change Params to extend self

--- lib/poke/modules/params.rb
+++ lib/poke/modules/params.rb
@@ -2,17 +2,15 @@ module Params
 
   MissingParamsError = Class.new(StandardError)
 
-  class << self
+  extend self
 
-    def check_params(params_given, params_required)
-      params_required.each do |param|
-        unless params_given.keys.include?(param)
-          raise MissingParamsError,
-            "parameters given do not include \":#{param}\""
-        end
+  def check_params(params_given, params_required)
+    params_required.each do |param|
+      unless params_given.keys.include?(param)
+        raise MissingParamsError,
+          "parameters given do not include \":#{param}\""
       end
     end
-
   end
 
 end

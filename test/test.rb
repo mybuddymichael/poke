1cec00fd327767b84b1d097603a852bf88fa896b
Michael Hanson <michael.b.hanson@gmail.com>
2011-07-26 07:21:51 -0700

    Move TestMap to its own file

--- test/test.rb
+++ test/test.rb
@@ -125,22 +125,3 @@ class TestCoordinates < MiniTest::Unit::TestCase
 
 end
 
-class TestMap < MiniTest::Unit::TestCase
-
-  def setup
-    @window = Window.new
-    @map_file = <<-eos
-      V....g...V
-      V....g...V
-      {'.'=>0, 'V'=>1, 'g'=>2}
-    eos
-
-    @map = Map.new(window: @window, map_file: @map_file, tileset: nil)
-  end
-
-  def test_extract_map_key_produces_a_hash
-    assert_equal({'.'=>0, 'V'=>1, 'g'=>2},
-                 @map.instance_variable_get(:@map_key))
-  end
-
-end

5a249d894244acd656204ad61b66d9167b74b3c0
Michael Hanson <michael.b.hanson@gmail.com>
2011-07-26 07:01:07 -0700

    Add hash test

--- test/test.rb
+++ test/test.rb
@@ -138,4 +138,9 @@ class TestMap < MiniTest::Unit::TestCase
     @map = Map.new(window: @window, map_file: @map_file, tileset:nil)
   end
 
+  def test_extract_map_key_produces_a_hash
+    assert_equal({'.'=>0, 'V'=>1, 'g'=>2},
+                 @map.instance_variable_get(:@map_key))
+  end
+
 end

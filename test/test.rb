6352083c521c8c95138cb716bc41977f212881cc
Michael Hanson <michael.b.hanson@gmail.com>
2011-07-26 07:15:22 -0700

    Formatting

--- test/test.rb
+++ test/test.rb
@@ -135,7 +135,7 @@ class TestMap < MiniTest::Unit::TestCase
       {'.'=>0, 'V'=>1, 'g'=>2}
     eos
 
-    @map = Map.new(window: @window, map_file: @map_file, tileset:nil)
+    @map = Map.new(window: @window, map_file: @map_file, tileset: nil)
   end
 
   def test_extract_map_key_produces_a_hash

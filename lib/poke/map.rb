3355eea03e9c264c40ae8bd1d66ae23b4a155f0f
Michael Hanson <michael.b.hanson@gmail.com>
2011-07-26 07:01:26 -0700

    Add Map#extract_map_key

--- lib/poke/map.rb
+++ lib/poke/map.rb
@@ -6,6 +6,13 @@ class Map
 
   def initialize(params)
     Params.check_params(params, PARAMS_REQUIRED)
+    @map_file = params(:map_file)
+
+    @map_key = extract_map_key
+  end
+
+  def extract_map_key
+    @map_file.last
   end
 
 end

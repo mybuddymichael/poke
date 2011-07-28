bbbc5a3cb8c0d203d5e725361d1d826a965c109f
Michael Hanson <michael.b.hanson@gmail.com>
2011-07-28 13:26:21 -0700

    Change mapr_file ivar to map_in_lines

--- lib/poke/map.rb
+++ lib/poke/map.rb
@@ -6,13 +6,13 @@ class Map
 
   def initialize(params)
     Params.check_params(params, PARAMS_REQUIRED)
-    @map_file = get_lines(params[:map_file])
+    @map_in_lines = get_lines(params[:map_file])
 
     @map_key = extract_map_key
   end
 
   def extract_map_key
-    eval(@map_file.last)
+    eval(@map_in_lines.last)
   end
 
 end

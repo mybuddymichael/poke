4a9d5338c945e4791644224d69f3a5d641075866
Michael Hanson <michael.b.hanson@gmail.com>
2011-07-28 13:27:04 -0700

    Use mapfile that is in place

--- test/test_map.rb
+++ test/test_map.rb
@@ -4,18 +4,12 @@ class TestMap < MiniTest::Unit::TestCase
 
   def setup
     @window = Window.new
-    @map_file = <<-eos
-      V....g...V
-      V....g...V
-      {'.'=>0, 'V'=>1, 'g'=>2}
-    eos
-
-    @map = Map.new(window: @window, map_file: @map_file, tileset: nil)
+    @map = @window.current_grid.instance_variable_get(:@map)
+    @map_key = @map.instance_variable_get(:@map_key)
   end
 
   def test_extract_map_key_produces_a_hash
-    assert_equal({'.'=>0, 'V'=>1, 'g'=>2},
-                 @map.instance_variable_get(:@map_key))
+    assert_equal({'.'=>0, 'V'=>1, 'g'=>2}, @map_key)
   end
 
 end

dac2fa6b8cd8acd45cc8ae1dd84c5efc1e8c90c6
Michael Hanson <michael.b.hanson@gmail.com>
2011-07-30 15:06:56 -0700

    Add Poke::Test definition to test_helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,3 +3,13 @@ SimpleCov.start
 
 require 'minitest/autorun'
 require_relative '../main'
+
+module Poke
+  class Test < MiniTest::Unit::TestCase
+
+    def self.test(name, &block)
+      define_method("test_#{name.gsub(/\W/,'_')}", &block) if block
+    end
+
+  end
+end

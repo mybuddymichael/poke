8a4c475c12251aaa6898f54a23993382cf699abc
Michael Hanson <michael.b.hanson@gmail.com>
2011-07-30 21:41:51 -0700

    Update #iterate_over_each_character_in_array_of_lines doc

--- lib/poke/modules/read_map.rb
+++ lib/poke/modules/read_map.rb
@@ -81,8 +81,8 @@ module ReadMap
   #
   # Examples
   #
-  #   iterate_over_each_character_in_array_of_lines(map_in_lines) do |x|
-  #     if x == "V"
+  #   iterate_over_each_character_in_array_of_lines(map_in_lines) do |y, x|
+  #     if @map_in_lines[y][x] == "V"
   #       print "V!"
   #     end
   #   end

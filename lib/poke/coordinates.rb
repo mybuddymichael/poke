b4867080127cccf9c5938099c79a7a07e32449e1
Michael Hanson <michael.b.hanson@gmail.com>
2011-08-03 22:35:12 -0700

    Add 'module Poke' to coordinates.rb

--- lib/poke/coordinates.rb
+++ lib/poke/coordinates.rb
@@ -1,17 +1,21 @@
-class Coordinates
+module Poke
 
-  def initialize(window, user)
-    @window, @user = window, user
-    @coordinate_text = Gosu::Font.new(@window, Media::FONT, 16)
-  end
+  class Coordinates
 
-  def update
-    @x, @y = @user.x, @user.y
-  end
+    def initialize(window, user)
+      @window, @user = window, user
+      @coordinate_text = Gosu::Font.new(@window, Media::FONT, 16)
+    end
 
-  def draw
-    @coordinate_text.draw("#{@x.to_s}, #{@y.to_s}", 416, 300, 1, 1, 1,
-                          Color::WHITE, :default)
+    def update
+      @x, @y = @user.x, @user.y
+    end
+
+    def draw
+      @coordinate_text.draw("#{@x.to_s}, #{@y.to_s}", 416, 300, 1, 1, 1,
+                            Color::WHITE, :default)
+    end
+
   end
 
 end

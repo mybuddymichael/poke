a5d301d31f49be7b51b279ed50a5c0637e16887f
Michael Hanson <michael.b.hanson@gmail.com>
2011-08-03 22:34:24 -0700

    Add 'module Poke' to controls.rb

--- lib/poke/controls.rb
+++ lib/poke/controls.rb
@@ -1,58 +1,62 @@
-class Controls
+module Poke
 
-  PARAMS_REQUIRED = [:window]
+  class Controls
 
-  BUTTONS = { up:     Gosu::KbUp,
-              down:   Gosu::KbDown,
-              left:   Gosu::KbLeft,
-              right:  Gosu::KbRight,
-              q:      Gosu::KbQ,
-              escape: Gosu::KbEscape }
+    PARAMS_REQUIRED = [:window]
 
-  def initialize(params)
-    Params.check_params(params, PARAMS_REQUIRED)
-    @window = params[:window]
-    @buttons_pressed = []
-  end
+    BUTTONS = { up:     Gosu::KbUp,
+                down:   Gosu::KbDown,
+                left:   Gosu::KbLeft,
+                right:  Gosu::KbRight,
+                q:      Gosu::KbQ,
+                escape: Gosu::KbEscape }
 
-  def press_button(button)
-    BUTTONS.each do |key, value|
-      if button == value
-        if key == :escape
-          toggle_pause
-        elsif key == :q
-          quit
-        else
-          @buttons_pressed.push(key)
+    def initialize(params)
+      Params.check_params(params, PARAMS_REQUIRED)
+      @window = params[:window]
+      @buttons_pressed = []
+    end
+
+    def press_button(button)
+      BUTTONS.each do |key, value|
+        if button == value
+          if key == :escape
+            toggle_pause
+          elsif key == :q
+            quit
+          else
+            @buttons_pressed.push(key)
+          end
         end
       end
     end
-  end
 
-  def release_button(button)
-    BUTTONS.each do |key, value|
-      if button == value
-        @buttons_pressed.delete(key)
+    def release_button(button)
+      BUTTONS.each do |key, value|
+        if button == value
+          @buttons_pressed.delete(key)
+        end
       end
     end
-  end
 
-  def last_button_pressed
-    @buttons_pressed.last
-  end
-
-  private
-
-  def toggle_pause
-    if @window.paused == false
-      @window.paused = true
-    else
-      @window.paused = false
+    def last_button_pressed
+      @buttons_pressed.last
     end
-  end
 
-  def quit
-    @window.close if @window.paused
+    private
+
+    def toggle_pause
+      if @window.paused == false
+        @window.paused = true
+      else
+        @window.paused = false
+      end
+    end
+
+    def quit
+      @window.close if @window.paused
+    end
+
   end
 
 end

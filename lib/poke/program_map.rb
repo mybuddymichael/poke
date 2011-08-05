8e26703b5f6620166376845586063b07903a9336
Michael Hanson <michael.b.hanson@gmail.com>
2011-08-05 07:17:38 -0700

    Add doc description for ProgramMap

--- lib/poke/program_map.rb
+++ lib/poke/program_map.rb
@@ -1,5 +1,7 @@
 module Poke
-
+  # +ProgramMap+ parses a map file and stores it as an Array, which contains
+  # paths and initial positions for {Program} instances. It will instantiate the
+  # programs as indicated by the map file.
   class ProgramMap
 
     include Poke::ReadMap

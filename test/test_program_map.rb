a35c7319eab86378348822da5844643b16da0ec6
Michael Hanson <michael.b.hanson@gmail.com>
2011-08-06 15:30:11 -0700

    Add test requirements for ProgramMap test

--- test/test_program_map.rb
+++ test/test_program_map.rb
@@ -2,6 +2,12 @@ require_relative 'test_helper'
 
 class TestProgramMap < Poke::Test
 
+  # Finds the upper-left corner of each path
+  # Finds the lower-right corner of each path
+  # Creates an array of each path's coordinates
+  #   [[[1, 1], [3, 3]], [[4, 5], [6, 8]]... etc
+  # Initializes Programs for each indicated character
+
   def setup
     @program_map =
       Poke::ProgramMap.new(map_file: 'media/grid_one/program_map.txt')

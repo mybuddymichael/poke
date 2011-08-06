d15276be27254d0abfcf31a7142d639f1884d071
Michael Hanson <michael.b.hanson@gmail.com>
2011-08-06 15:29:23 -0700

    Add def for Character class

--- lib/poke/character.rb
+++ lib/poke/character.rb
@@ -1,5 +1,7 @@
 module Poke
-
+  # +Character+ is a base class for both non-player and player characters. It
+  # contains various methods for moving, updating, drawing, and collision
+  # detection.
   class Character
   end
   

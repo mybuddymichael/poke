4e52e57bdd0952edb7f743cca5b6dbde752360c5
Michael Hanson <michael.b.hanson@gmail.com>
2011-08-06 14:09:27 -0700

    Add TestProgramMap#setup

--- test/test_program_map.rb
+++ test/test_program_map.rb
@@ -2,5 +2,10 @@ require_relative "test_helper"
 
 class TestProgramMap < Poke::Test
 
+  def setup
+    @program_map =
+      Poke::ProgramMap.new(map_file: 'media/grid_one/program_map.txt')
+  end
+
 end
 

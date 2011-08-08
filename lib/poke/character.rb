770f7f89eafe0a82a83a9adb7b206f9ad40a8994
Michael Hanson <michael.b.hanson@gmail.com>
2011-08-08 07:11:20 -0700

    Add base skeleton structure to Character

--- lib/poke/character.rb
+++ lib/poke/character.rb
@@ -3,6 +3,16 @@ module Poke
   # contains various methods for moving, updating, drawing, and collision
   # detection.
   class Character
+
+    PARAMS_REQUIRED = [:window, :x, :y]
+
+    attr_reader :x
+    attr_reader :y
+    attr_reader :direction
+
+    def initialize(params = {})
+    end
+
   end
   
 end

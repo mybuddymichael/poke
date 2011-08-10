829d7befc5166908ed6564d68b0f4c763005d9bf
Michael Hanson <michael.b.hanson@gmail.com>
2011-08-10 14:14:19 -0700

    Add camera.rb along with doc description

---
+++ lib/poke/camera.rb
@@ -0,0 +1,8 @@
+module Poke
+  # A Camera object provides a viewport to the game window. It allows a player
+  # character to move in an area of greater size than the window and still let
+  # the user see the movement of the character on the screen.
+  class Camera
+  end
+
+end

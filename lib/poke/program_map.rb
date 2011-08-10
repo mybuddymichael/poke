8e86da69b6a00be1de71a414872b18ac5506bc7d
Michael Hanson <michael.b.hanson@gmail.com>
2011-08-09 22:56:32 -0700

    Change ProgramMap to NPCMap

--- lib/poke/program_map.rb
+++ lib/poke/program_map.rb
@@ -2,7 +2,7 @@ module Poke
   # +ProgramMap+ parses a map file and stores it as an Array, which contains
   # paths and initial positions for {Program} instances. It will instantiate the
   # programs as indicated by the map file.
-  class ProgramMap
+  class NPCMap
 
     include Poke::ReadMap
 

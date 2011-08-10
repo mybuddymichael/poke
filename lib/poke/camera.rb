8c3afc659e1e7eeaf4fb5fc9a8355950f0c43c30
Michael Hanson <michael.b.hanson@gmail.com>
2011-08-10 14:22:16 -0700

    Add PARAMS_REQUIRED and #initialize with docs

--- lib/poke/camera.rb
+++ lib/poke/camera.rb
@@ -3,6 +3,18 @@ module Poke
   # character to move in an area of greater size than the window and still let
   # the user see the movement of the character on the screen.
   class Camera
+
+    # Params required at initialization.
+    PARAMS_REQUIRED = [:player]
+
+    # Creates a Camera object.
+    #
+    # params - The Hash of Symbol options.
+    #   :player - The Player object that the Camera is tracking.
+    #
+    # Returns a Camera object.
+    def initialize(params = {})
+    end
   end
 
 end

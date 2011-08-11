73efabadce2c296c24bc034c3d64ff0d21c9fadb
Michael Hanson <michael.b.hanson@gmail.com>
2011-08-11 07:09:01 -0700

    Add a mock and some setup to test_camera

--- test/test_camera.rb
+++ test/test_camera.rb
@@ -2,7 +2,19 @@ require_relative 'test_helper'
 
 class TestCamera < Poke::Test
 
+  class Player
+    attr_reader :x, :y
+
+    def initialize
+      @x = 32
+      @y = 64
+    end
+  end
+
   def setup
+    @window = TWindow.new
+    @player = Player.new
+    @camera = Poke::Camera.new(window: @window, player: @player)
   end
 
 end

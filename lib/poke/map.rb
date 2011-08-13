002431d226b0ff22886659f9eb1954f4b83cb7bb
Michael Hanson <michael.b.hanson@gmail.com>
2011-08-13 12:58:49 -0700

    Add doc for Map#initialize

--- lib/poke/map.rb
+++ lib/poke/map.rb
@@ -6,6 +6,10 @@ module Poke
     include Poke::ReadMap
 
 
+    # Creates a Map object.
+    #
+    # params - A Hash of Symbol options.
+    #   :map_file - An ASCII file that represents the Map.
     def initialize(params = {})
     end
 

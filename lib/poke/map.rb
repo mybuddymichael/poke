7e9a83e733e84da5405fe48136aac929525ce42a
Michael Hanson <michael.b.hanson@gmail.com>
2011-08-13 15:20:03 -0700

    Add #get_array_of_lines_from_file to Map

--- lib/poke/map.rb
+++ lib/poke/map.rb
@@ -17,5 +17,21 @@ module Poke
       @map_key      = extract_map_key(@map_in_lines)
     end
 
+    # Creates an array of strings corresponding to the lines of the file passed in
+    # as an argument. Useful for traversing a map using Y-then-X notation, e.g.
+    # @map_in_columns[y][x].
+    #
+    # file - A String containing the path to the file to be converted.
+    #
+    # Examples
+    #
+    #   get_array_of_lines_from_file("map.txt")
+    #   # => ["VV..gg", "..VVgg", "..VV.."]
+    #
+    # Returns an Array of Strings.
+    def get_array_of_lines_from_file(file)
+      File.readlines(file).map { |line| line.chomp }
+    end
+
   end
 end

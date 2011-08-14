985249afe758e27ef9be82916df100b223b90ea3
Michael Hanson <michael.b.hanson@gmail.com>
2011-08-14 09:35:55 -0700

    Fixed TestMap referencing wrong map image key variable

--- test/test_map.rb
+++ test/test_map.rb
@@ -4,11 +4,11 @@ class TestMap < MiniTest::Unit::TestCase
 
   def setup
     @map = Poke::Map.new(map_file: 'media/grid_one/map.txt')
-    @map_key = @map.instance_variable_get(:@map_key)
   end
 
   def test_extract_map_key_produces_a_hash
-    assert_equal({'.'=>0, 'V'=>1, 'g'=>2}, @map_key)
+    @map_image_key = @map.instance_variable_get(:@map_image_key)
+    assert_equal({'.'=>0, 'V'=>1, 'g'=>2}, @map_image_key)
   end
 
 end

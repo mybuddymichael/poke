4a3092a41937e7d9f2d36d5c571649fa6a8363c2
Michael Hanson <michael.b.hanson@gmail.com>
2011-08-15 22:36:18 -0700

    PauseScreen now uses WIDTH and HEIGHT constants

--- lib/poke/pausescreen.rb
+++ lib/poke/pausescreen.rb
@@ -2,20 +2,18 @@ module Poke
 
   class PauseScreen
 
-    PARAMS_REQUIRED = [:window, :width, :height]
+    PARAMS_REQUIRED = [:window]
 
     def initialize(params)
       Params.check_params(params, PARAMS_REQUIRED)
       @window = params[:window]
-      @width  = params[:width]
-      @height = params[:height]
 
       @pause_button = Gosu::Image.new(@window, 'media/pause_button.png', false)
       @press_q      = Gosu::Image.new(@window, 'media/press_q.png', false)
     end
 
     def draw
-      draw_rect(@width, @height, Color::TRANS_BLACK,
+      draw_rect(Poke::Math::WIDTH, Poke::Math::HEIGHT, Color::TRANS_BLACK,
                 ZOrder::PAUSE_BACKGROUND)
       @pause_button.draw(160, 144, ZOrder::PAUSE_BUTTON)
       @press_q.draw(170, 296, ZOrder::PAUSE_BUTTON)
